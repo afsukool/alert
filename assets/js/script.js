24e07b49e438777c461874824bc5cab9d051a91b
4ZUR30R34P3R <91094394+afsukool@users.noreply.github.com>
2024-10-23 09:49:29 +0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,42 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const form = document.querySelector("form");
-  const loadingMessage = document.querySelector(".loading");
-  const errorMessage = document.querySelector(".error-message");
-  const sentMessage = document.querySelector(".sent-message");
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("form");
+    const loadingMessage = document.querySelector(".loading");
+    const errorMessage = document.querySelector(".error-message");
+    const sentMessage = document.querySelector(".sent-message");
 
-  form.addEventListener("submit", function(event) {
-    event.preventDefault();
-    
-    loadingMessage.style.display = "block";
-    errorMessage.style.display = "none";
-    sentMessage.style.display = "none";
+    // Function to handle form submission
+    const handleSubmit = (event) => {
+        event.preventDefault();
 
-    const formData = new FormData(form);
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", form.action, true);
-    xhr.setRequestHeader("Accept", "application/json");
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === XMLHttpRequest.DONE) {
-        loadingMessage.style.display = "none";
-        if (xhr.status === 200) {
-          sentMessage.style.display = "block";
-          form.reset();
-        } else {
-          errorMessage.style.display = "block";
-          errorMessage.textContent = "There was a problem submitting your form. Please try again.";
-        }
-      }
+        // Show loading message and hide other messages
+        loadingMessage.style.display = "block";
+        errorMessage.style.display = "none";
+        sentMessage.style.display = "none";
+
+        const formData = new FormData(form);
+        const xhr = new XMLHttpRequest();
+        
+        xhr.open("POST", form.action, true);
+        xhr.setRequestHeader("Accept", "application/json");
+
+        // Handle the response from the server
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                loadingMessage.style.display = "none";
+                if (xhr.status === 200) {
+                    sentMessage.style.display = "block";
+                    form.reset();
+                } else {
+                    errorMessage.style.display = "block";
+                    errorMessage.textContent = "There was a problem submitting your form. Please try again.";
+                }
+            }
+        };
+
+        // Send form data
+        xhr.send(formData);
     };
-    xhr.send(formData);
-  });
+
+    // Attach the submit event listener to the form
+    form.addEventListener("submit", handleSubmit);
 });
